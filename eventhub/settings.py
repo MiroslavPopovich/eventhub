b8dd00de0d73b0d1c85168a025b9e9c53e985564
MiroslavPopovich <miroslav_popovich@icloud.com>
2022-04-14 21:36:00 +0100

    Fix0.1

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4kk(35+d(wbf*w$pr9^+@xcijyvm8qcfx6my$gj3zdq+26-ov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
