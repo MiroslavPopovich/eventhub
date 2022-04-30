942f7b9ead9a5187b8eb2777dadd26ed320d5f07
MiroslavPopovich <miroslav_popovich@icloud.com>
2022-04-30 15:24:00 +0100

    New Cloudinary settings

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -165,4 +165,5 @@ cloudinary.config(
   cloud_name = os.getenv('CLOUDINARY_GLOUD_NAME', None), 
   api_key = os.getenv('CLOUDINARY_API_KEY', None),
   api_secret = os.getenv('CLOUDINARY_API_SECRET', None),
+  secure = True,
 )
